266438ceb53b83d50d7d7fe8b41c386aa510d2bc
@Roche <40134825+rocje@users.noreply.github.com>
2021-10-17 21:14:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def build_model(comp_country_code):
         daily_seasonality=True,
         # weekly_seasonality=True,
         changepoint_prior_scale=1,
-    #)
+    )
     if comp_country_code:
         m.add_country_holidays(country_name=comp_country_code)
     return m
